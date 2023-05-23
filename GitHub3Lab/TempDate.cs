b0c13196f1ee3faa06ff7d89e62e392945fd2ced
Jana1324 <ja_na00@mail.ru>
2023-05-23 22:25:39 +0300

    Create TempDate.cs

---
+++ GitHub3Lab/TempDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub3Lab
+{
+    public class TempDate
+    {
+        public DateTime Date { get; set; }
+        public double MaxTemperature { get; set; }
+        public double MinTemperature { get; set; }
+        public double AvgTemperature { get; set; }
+    }
+}
