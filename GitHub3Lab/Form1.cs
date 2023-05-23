86f24f054a7c4d69aca619994117350a296a37f1
marinapak03 <106282289+marinapak03@users.noreply.github.com>
2023-05-23 22:15:32 +0300

    add method DisplayRunDay

--- GitHub3Lab/Form1.cs
+++ GitHub3Lab/Form1.cs
@@ -18,6 +18,9 @@ namespace GitHub3Lab
             InitializeComponent();
         }
 
+        private RunProcessing runPocessing;
+        List<string> OutputRunList = new List<string>(); // список всех дней
+
         private void chart1_Click(object sender, EventArgs e)
         {
 
@@ -26,6 +29,31 @@ namespace GitHub3Lab
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public void DisplayRunDay()
+        {
+            List<RunDay> runDataList = runPocessing.GetRunDayList();
+
+            if (runDataList.Count > 0)
+            {
+                string output = "";
+
+                foreach (var item in runDataList)
+                {
+                    output = string.Format(" День {0}: Длительность: {1} мин, Расстояние: {2} км, Максимальная скорость: {3} км/ч, " +
+                        "Минимальная скорость: {4} км/ч, Средняя скорость: {5} км/ч, Средний пульс: {6} уд/мин\n", item.DayOfWeek,
+                        item.Duration, item.Distance, item.MaxSpeed, item.MinSpeed, item.AvgSpeed,
+                        item.AvgPulse);
+                    listBoxRun.Items.Add(output);
+                    OutputRunList.Add(output);
+                }
+                DistanceCalc.Text += runPocessing.CalculateWeekendDistanceSum().ToString() + " км";
+            }
+            else
+            {
+                MessageBox.Show("Файл не содержит данных!");
+            }
         }
 
         private void buttonOpenFileRun_Click(object sender, EventArgs e)
