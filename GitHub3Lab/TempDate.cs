c5ef8228e908633a45e750ccf5caccec3d46a933
Jana1324 <ja_na00@mail.ru>
2023-05-23 22:26:56 +0300

    Read FileTemp

--- GitHub3Lab/TempDate.cs
+++ GitHub3Lab/TempDate.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GitHub3Lab
 {
@@ -13,4 +15,45 @@ namespace GitHub3Lab
         public double MinTemperature { get; set; }
         public double AvgTemperature { get; set; }
     }
-}
+    // чтение данных из файла и их обработка
+    public class TempProcessing
+    {
+        private List<TempDate> tempDayList;
+        private string filePath;
+
+        public TempProcessing(string filePath)
+        {
+            this.filePath = filePath;
+            tempDayList = new List<TempDate>();
+        }
+
+        // метод для чтения данных из файла
+        public void ReadFromFile()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (var item in lines)
+                {
+                    string[] values = item.Split(' ');
+
+                    TempDate tempData = new TempDate
+                    {
+                        Date = DateTime.Parse(values[0]),
+                        MaxTemperature = double.Parse(values[1]),
+                        MinTemperature = double.Parse(values[2]),
+                        AvgTemperature = double.Parse(values[3])
+                    };
+
+                    tempDayList.Add(tempData);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка при чтении. Проверьте данные на корректность.");
+
+            }
+        }
+    }
+    }
